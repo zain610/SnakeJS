4d166f264ca4fd3640a7dc24c98e6e20246d34f5
zain610 <znshroff@gmail.com>
2020-08-18 18:16:14 +1000

    working on creating snakes

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,60 @@
-const width = window.innerWidth
-const height = window.innerHeight
-const main = () => {
+window.addEventListener('DOMContentLoaded', (e) => {
+  const width = window.innerWidth
+  const height = window.innerHeight
+  var canvas = document.getElementById('myCanvas')
+  let ctx = canvas.getContext("2d")
+  var x = canvas.width / 2
+  var y = canvas.height / 2
+  webkitRequestAnimationFrame
+  let counter = 0
 
-  createBoard()
+  const draw = () => {
+    //do something
+    x += 10
+    y += 10
+    counter += 1
+    createSnake(canvas, ctx, x, y)
 
-
-}
-
-const createBoard = (x = 100, y = 100) => {
-  var app = document.getElementById('app')
-  const board = document.createElement('div')
-  console.log(width, height)
-  //set the styling for the board
-  board.style.width = '20rem'
-  board.style.height = '20rem'
-  board.style.margin = 'auto'
-  board.style.alignContent = 'center'
-  board.style.backgroundColor = 'black'
-
-  board.appendChild(createSnake(0))
-  board.appendChild(createSnake(0.05))
-  board.appendChild(createSnake(1.0))
+  }
+  setInterval(draw, 1000)
 
 
-  app.appendChild(board)
-}
 
-const createSnake = (counter) => {
-  const snake = document.createElement('div')
-  snake.style.width = '1rem'
-  snake.style.height = '1rem'
-  snake.style.backgroundColor = 'green'
-  snake.style.position = 'relative'
-  snake.style.top = `${counter}rem`
-  snake.style.left = '10rem'
-  return snake
 
-}
+  const main = () => {
+
+    createSnake(ctx)
+    createBoard()
+
+
+  }
+
+  const createBoard = (x = 100, y = 100) => {
+    var app = document.getElementById('app')
+    const board = document.createElement('div')
+    console.log(width, height)
+    //set the styling for the board
+    board.style.width = '20rem'
+    board.style.height = '20rem'
+    board.style.margin = 'auto'
+    board.style.alignContent = 'center'
+    board.style.backgroundColor = 'black'
+
+    board.appendChild(createSnake(0))
+
+
+    app.appendChild(board)
+  }
+
+  const createSnake = (canvas, context, x, y) => {
+    //create snake
+    console.log(canvas)
+    context.beginPath()
+    context.rect(x, y, 16, 16)
+    context.fillStyle = "#FF000"
+    context.fill()
+    context.closePath()
+
+
+  }
+})
