a3330d5e4b9829b44b454bba34136a59a457e3b9
zain610 <znshroff@gmail.com>
2020-08-19 14:51:25 +1000

    moving snake achieved

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,9 @@ const directions = {
   RIGHT_ARROW: [1, 0],
   LEFT_ARROW: [-1, 0],
 }
-
+const boardColor = '#eee'
+const snakeColor = 'lightblue'
+const snakeBorder = '#333'
 
 window.addEventListener('DOMContentLoaded', (e) => {
   const width = window.innerWidth
@@ -15,11 +17,12 @@ window.addEventListener('DOMContentLoaded', (e) => {
   var x = canvas.width / 2
   var y = canvas.height / 2
   // direction of snake
-  let xSpeed = 1
+  let xSpeed = 18
   let ySpeed = 0
 
   //body of snake -- keep track of all components of the snake's body
-  let body = [{ x, y }, { x: x - 15, y: y }]
+  //keep snake parts 18pts apart 
+  let body = [{ x, y }, { x: x - 18, y: y }]
 
   const updateDir = (x, y) => {
     xSpeed = x
@@ -29,16 +32,18 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
   const draw = () => {
     //do something
-    x += 16
+    clear_board(canvas, ctx)
     for (let pos of body) {
       let { x, y } = pos
       drawSnake(ctx, x, y)
     }
+
+    moveSnake()
+
   }
-  setInterval(draw, 1000)
-
-
-
+  setInterval(() => {
+    draw()
+  }, 1000)
   /**
    * 
    * @param {*} canvas canvas element from DOM
@@ -47,10 +52,23 @@ window.addEventListener('DOMContentLoaded', (e) => {
    * @param {*} y y coordinate of the snake
    */
   const drawSnake = (context, x, y) => {
-    context.clearRect(0, 0, width, height)
-    context.rect(x, y, 16, 16)
-    context.fillStyle = "#FF000"
-    context.fill()
+
+    context.fillStyle = snakeColor
+    context.strokeStyle = snakeBorder
+    context.fillRect(x, y, 16, 16)
+    context.strokeRect(x, y, 16, 16)
+  }
+  //move snake
+  const moveSnake = () => {
+    const head = { x: body[0].x + xSpeed, y: body[0].y + ySpeed }
+    body.unshift(head)
+    body.pop()
+    console.log(body)
+  }
+  // draw a border around the canvas
+  function clear_board(canvas, context) {
+    console.log('clear')
+    context.clearRect(0, 0, canvas.width, canvas.height);
   }
 
 
@@ -58,12 +76,16 @@ window.addEventListener('DOMContentLoaded', (e) => {
     switch (e.key) {
       case 'ArrowUp':
         console.log('ArrowUp')
+        break
       case 'ArrowDown':
         console.log('ArrowDown')
+        break
       case 'ArrowRight':
         console.log('ArrowRight')
+        break
       case 'ArrowLeft':
         console.log('ArrowLeft')
+        break
     }
   })
 
