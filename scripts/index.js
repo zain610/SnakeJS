977d0aa2172d418ce6ae76268a2fc9c9af63c2fb
zain610 <znshroff@gmail.com>
2020-08-19 16:18:16 +1000

    added validation for edges overflow

--- scripts/index.js
+++ scripts/index.js
@@ -18,11 +18,13 @@ window.addEventListener('DOMContentLoaded', (e) => {
   var y = canvas.height / 2
   // direction of snake
   let xSpeed = 18
-  let ySpeed = 0
+  let ySpeed = 18
 
   //body of snake -- keep track of all components of the snake's body
   //keep snake parts 18pts apart 
-  let body = [{ x, y }, { x: x - 18, y: y }]
+  let body = [{ x, y }]
+
+  let dir = directions.RIGHT_ARROW //by default move right
 
   const updateDir = (x, y) => {
     xSpeed = x
@@ -37,13 +39,13 @@ window.addEventListener('DOMContentLoaded', (e) => {
       let { x, y } = pos
       drawSnake(ctx, x, y)
     }
+    validateSnakeMovement()
 
-    moveSnake()
 
   }
   setInterval(() => {
     draw()
-  }, 1000)
+  }, 100)
   /**
    * 
    * @param {*} canvas canvas element from DOM
@@ -58,9 +60,11 @@ window.addEventListener('DOMContentLoaded', (e) => {
     context.fillRect(x, y, 16, 16)
     context.strokeRect(x, y, 16, 16)
   }
-  //move snake
-  const moveSnake = () => {
-    const head = { x: body[0].x + xSpeed, y: body[0].y + ySpeed }
+  //move snake -- most important method 
+  const moveSnake = (dirX = 0, dirY = -1) => {
+    let dx = xSpeed * dirX
+    let dy = ySpeed * dirY
+    const head = { x: body[0].x + dx, y: body[0].y + dy }
     body.unshift(head)
     body.pop()
     console.log(body)
@@ -71,22 +75,40 @@ window.addEventListener('DOMContentLoaded', (e) => {
     context.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-
+  //add keyboard controls
   window.addEventListener('keydown', e => {
+    //convert this into an if else statement and check if the snake is currently going in the same direction. 
     switch (e.key) {
       case 'ArrowUp':
         console.log('ArrowUp')
+        dir = directions.UP_ARROW
         break
       case 'ArrowDown':
         console.log('ArrowDown')
+        dir = directions.DOWN_ARROW
         break
       case 'ArrowRight':
         console.log('ArrowRight')
+        dir = directions.RIGHT_ARROW
         break
       case 'ArrowLeft':
         console.log('ArrowLeft')
+        dir = directions.LEFT_ARROW
         break
     }
   })
+  const validateSnakeMovement = () => {
+    if (body[0].x < -1) {
+      body[0].x = canvas.width
+    } else if (body[0].x > canvas.width) {
+      body[0].x = 0
+    } else if (body[0].y < 0) {
+      body[0].y = canvas.height
+    } else if (body[0].y > canvas.height) {
+      body[0].y = 0
+    } else {
+      moveSnake(dir[0], dir[1])
+    }
+  }
 
 })
